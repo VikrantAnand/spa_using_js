d60e5e5ab9d30648f7c46c2aba8aef993faa530b
VikrantAnand <85404884+VikrantAnand@users.noreply.github.com>
2022-05-12 17:58:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,142 @@
+let subjectEl1 = document.getElementById('subject1');
+let facultyEl1 = document.getElementById('faculty1');
+let contactEl1 = document.getElementById('contact1');
+let indexEl1 = document.getElementById('index1');
+
+let subjectEl2 = document.getElementById('subject2');
+let facultyEl2 = document.getElementById('faculty2');
+let contactEl2 = document.getElementById('contact2');
+let indexEl2 = document.getElementById('index2');
+
+let subjectEl3 = document.getElementById('subject3');
+let facultyEl3 = document.getElementById('faculty3');
+let contactEl3 = document.getElementById('contact3');
+let indexEl3 = document.getElementById('index3');
+
+let subjectEl4 = document.getElementById('subject4');
+let facultyEl4 = document.getElementById('faculty4');
+let contactEl4 = document.getElementById('contact4');
+let indexEl4 = document.getElementById('index4');
+
+
+let subjectPage = document.getElementById('subjectp');
+let facultyPage = document.getElementById('facultyp');
+let contactPage = document.getElementById('contactp');
+let indexPage = document.getElementById('indexp');
+
+subjectEl1.addEventListener('click',function(){
+    subjectPage.style.display = 'block';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Subject Clicked');
+});
+facultyEl1.addEventListener('click',function(){
+    facultyPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Faculty Clicked');
+});
+contactEl1.addEventListener('click',function(){
+    contactPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    console.log('Contact Clicked');
+});
+indexEl1.addEventListener('click',function(){
+    indexPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Index Clicked');
+});
+
+subjectEl2.addEventListener('click',function(){
+    subjectPage.style.display = 'block';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Subject Clicked');
+});
+facultyEl2.addEventListener('click',function(){
+    facultyPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Faculty Clicked');
+});
+contactEl2.addEventListener('click',function(){
+    contactPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    console.log('Contact Clicked');
+});
+indexEl2.addEventListener('click',function(){
+    indexPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Index Clicked');
+});
+
+subjectEl3.addEventListener('click',function(){
+    subjectPage.style.display = 'block';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Subject Clicked');
+});
+facultyEl3.addEventListener('click',function(){
+    facultyPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Faculty Clicked');
+});
+contactEl3.addEventListener('click',function(){
+    contactPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    console.log('Contact Clicked');
+});
+indexEl3.addEventListener('click',function(){
+    indexPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Index Clicked');
+});
+
+subjectEl4.addEventListener('click',function(){
+    subjectPage.style.display = 'block';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Subject Clicked');
+});
+facultyEl4.addEventListener('click',function(){
+    facultyPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Faculty Clicked');
+});
+contactEl4.addEventListener('click',function(){
+    contactPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    indexPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    console.log('Contact Clicked');
+});
+indexEl4.addEventListener('click',function(){
+    indexPage.style.display = 'block';
+    subjectPage.style.display = 'none';
+    facultyPage.style.display = 'none';
+    contactPage.style.display = 'none';
+    console.log('Index Clicked');
+});
+
